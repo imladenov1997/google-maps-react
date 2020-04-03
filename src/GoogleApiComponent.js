b0e36053b68359e69ab41af88e4d019e0ac40ebb
Ivo Mladenov <imladenov1997@gmail.com>
2020-04-03 20:31:26 +0100

    Revert "Merge pull request #375 from JeremiahChurch/master"
    This reverts commit c54f4705a7c5295ef7b783c88e0585b5dc76f73c, reversing
    changes made to 33f69e96faad11f8c09139601536eac7b0b4e45a.

--- src/GoogleApiComponent.js
+++ src/GoogleApiComponent.js
@@ -55,7 +55,7 @@ export const wrapper = input => WrappedComponent => {
       this.mapRef=React.createRef();
     }
 
-    UNSAFE_componentWillReceiveProps(props) {
+    componentWillReceiveProps(props) {
       // Do not update input if it's not dynamic
       if (typeof input !== 'function') {
         return;
